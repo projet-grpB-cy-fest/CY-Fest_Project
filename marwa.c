f8d5c01ff88f1c7fd86f891f917cb86553079542
Kahina <166177981+Kahinaaa2@users.noreply.github.com>
2024-04-22 12:52:32 +0200

    Add files via upload

---
+++ marwa.c
@@ -0,0 +1,36 @@
+typedef struct {
+    int nbsiege;
+    float prix;
+    int numero;
+    int nbsiege_indispo;
+} Siege;
+
+typedef struct {
+    int nom[50];
+    Siege categorieA;
+    Siege categorieB;
+    Siege categorieC;
+} Salle;
+
+
+void configuration_salle(Salle *salle) {
+    printf("Entrez le nombre de sièges pour la catégorie A : ");
+    scanf("%d", &salle->categorieA.nbsiege);
+    printf("Entrez le prix par siège pour la catégorie A : ");
+    scanf("%f", &salle->categorieA.prix);
+
+    printf("Entrez le nombre de sièges pour la catégorie B : ");
+    scanf("%d", &salle->categorieB.nbsiege);
+    printf("Entrez le prix par siège pour la catégorie B : ");
+    scanf("%f", &salle->categorieB.prix);
+
+  printf("Entrez le nombre de sièges pour la catégorie C : ");
+      scanf("%d", &salle->categorieC.nbsiege);
+      printf("Entrez le prix par siège pour la catégorie C : ");
+      scanf("%f", &salle->categorieC.prix);
+  }
+
+
+float calculerRatio(Salle salle){
+    return (salle.categorieA.nbsiege_indispo + salle.categorieB.nbsiege_indispo + salle.categorieC.nbsiege_indispo)/(salle.categorieA.nbsiege + salle.categorieB.nbsiege + salle.categorieC.nbsiege);
+}
