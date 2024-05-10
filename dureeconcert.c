a6bce00fa9b2ff2e6b9cbf2522c0486da8acd31e
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-05-10 18:20:13 +0200

    Add files via upload

---
+++ dureeconcert.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+void constructeur_date(struct tm date){//fonction qui demande à l'utilisateur le jour, l'heure et la minute du début ou de la fin de l'activité
+//sturct tm date: structure contenant les informations nécessaire pour déterminer le temps
+    
+    do{//vérifie que la valeur entrée est correct
+        printf("\nJour: ");
+        scanf("%d",&date.tm_mday);
+    }while(date.tm_hour<1 || date.tm_hour>31);
+
+    do{//vérifie que la valeur entrée est correct
+        printf("\nHeure: ");
+        scanf("%d",&date.tm_hour);
+    }while(date.tm_hour<0 || date.tm_hour>23);
+
+    do{//vérifie que la valeur entrée est correct
+        printf("\nMinute: ");
+        scanf("%d",&date.tm_min);
+    }while(date.tm_min<0 || date.tm_min>59);
+
+    //initialise les autres valeurs à 0
+    date.tm_year=0;
+    date.tm_mon=0;
+    date.tm_sec=0;
+}
+
+void affiche_date(struct tm date){//fonction qui affiche la date entré
+    printf("\nle %d à %d:%d\n",date.tm_mday,date.tm_hour,date.tm_min);
+}
+
+void validation_date(struct tm date){//fonction afficher pour l'utilisateur, s'assure si le choix de l'utilisateur lui convient
+    int date_correct;
+    while(date_correct!=1){//continue jusqu'à ce que la date convient à l'utilisateur
+        constructeur_date(date);//fonction pour créer la date
+        printf("\nCette date vous correspond elle?: ");
+        affiche_date(date);//affiche la date entrée par l'utilisateur
+        do{
+            printf("\n 1|OUI     2|NON\n");
+            scanf("%d",&date_correct); 
+        }while(date_correct<1 || date_correct>2);//recommence si la valeur entrée ne correspond pas aux choix
+    }
+
+}
+
+int duree_activite(struct tm debut, struct tm fin, struct tm instant_t){//Indique la fin des reservation, retourne 1 si le concert est fini/commencé, dans ce cas aucune réservation n'est possible puisque le concert est fini/commencé. Sinon retourne 0.
+//instant_t pourrait étre la valeur entrez par l'utilisateur pour indiquer l'heure de reservation 
+    if(instant_t.tm_hour>=fin.tm_hour || instant_t.tm_hour<=debut.tm_hour){
+        printf("fin");
+        return 1;
+    }
+    else{
+        printf("libre");
+        return 0;
+    }
+
+}
+
+int main( ){
+    struct tm date_debut;
+    struct tm date_fin;
+    struct tm instant;//structure de date
+    time_t t;//time_t: variable qui indique le nombre de seconde
+
+    //l'utilisateur rentre le début et la fin de l'activité
+    validation_date(date_debut);
+    validation_date(date_fin);
+
+    //demande à l'utilisateur la date de réservation (inserer une fonction pour comparer la date avec celle de l'activité choisie)
+    validation_date(instant);
+
+    //vérifie si la réservation est possible
+    duree_activite(date_debut,date_fin,instant);
+
+    return 0;
+} 
+//source: http://sdz.tdct.org/sdz/ime-h-et-ses-fonctions.html
+//        https://koor.fr/C/ctime/ctime.wp
--- testmenu.c
+++ testmenu.c
@@ -1,21 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int val,rep;
+void choixmenu(){
+    int mode,choix_manager,choix_festivalier;
+
+//entrez le mode:
+    printf("\n1-MANAGER \n2-FESTIVALIER ");
+
+//boucle vérifie si la valeur entrez est correct
     do{
-        printf("1 ou 2: ");
-        val=1;
-        if(val==1){
-            printf("\n1,2 ou 3: ");
-            scanf("%d",rep);
-            if(rep==1){
-                printf("1");
-            }
-            else if(rep==2){
-                printf("2");
-            }
+        printf("\n\nEntrez votre choix: ");
+        scanf("%d",&mode);
+    }while(mode<1||mode>2);
+
+//mode manager:
+    if(mode==1){
+
+    //menu
+        printf("\n1-Creer une salle \n2-Reserver une salle \n3-Modifier la configuration d'une salle \n4-Etat de la salle \n5-Retour au menu principal ");
+    
+    //boucle vérifie si la valeur entrez est correct
+        do{
+            printf("\n\nEntrez votre choix: ");
+            scanf("%d",&choix_manager);
+        }while(choix_manager<1||choix_manager>5);
+    
+    //boucle switch case pour chaque valeur entrez
+        switch(choix_manager){
+            case 5:
+                //retour menu principal
+                choixmenu();
+            break;
+
+            case 1:
+                //Creer une salle
+                printf("\nCreation salle: ");
+                //appelle fonction correspondante
+            break;
+
+            case 2:
+                //Reserver salle
+                printf("\nReservation salle: ");
+                //appelle fonction correspondante
+            break;
+
+            case 3:
+                //Configurer salle
+                printf("\nConfiguration salle: ");
+                //appelle fonction correspondante
+            break;
+
+            case 4:
+                //Etat de la salle
+                printf("\nEtat salle: ");
+                //appelle fonction correspondante
+            break;
+
+            default:
+                //message d'erreur ou cas ou
+                printf("\nerreur saisie");
+            break;
         }
-    }while(rep!=3);
-    return 0;
+    }
+
+//mode festivalier:
+    if(mode==2){
+
+    //menu
+        printf("\n1-Afficher les salles \n2-Reservation \n3-Retour menu principal ");
+   
+    //boucle vérifie si la valeur entrez est correct
+        do{
+            printf("\n\nEntrez votre choix: ");
+            scanf("%d",&choix_festivalier);
+        }while(choix_festivalier<1||choix_festivalier>3);
+    
+    //boucle switch case pour chaque valeur entrez
+        switch(choix_festivalier){
+            case 3:
+                //retour menu principal
+                choixmenu();
+            break;
+            
+            case 1:
+                //Afficher les salles
+                printf("\nAffichage salles: ");
+                //appelle fonction correspondante
+            break;
+            
+            case 2:
+                //Réservation
+                printf("\nReservation siege: ");
+                //appelle fonction correspondante
+            break;
+            
+            default:
+                //message d'erreur ou cas ou
+                printf("\nerreur saisie");
+            break;
+        }
+    }
+
+}
+
+int main(){
+//appele fonction
+choixmenu();
+return 0;
 }
