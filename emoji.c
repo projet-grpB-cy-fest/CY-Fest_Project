6cfc54a1bf6068c78f6d85980a8f7a7e59716849
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-05-15 14:58:23 +0200

    Add files via upload

---
+++ emoji.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main (void){
+
+ printf("%s", "\U0001f600"); // :-)
+ 
+ //source: https://stackoverflow.com/questions/73173217/how-do-i-add-an-emoji-on-my-printing-text-using-c-language
+ //https://www.developpez.net/forums/d311883/c-cpp/c/smiley-langage-c/
+  
+return 0;
+}
