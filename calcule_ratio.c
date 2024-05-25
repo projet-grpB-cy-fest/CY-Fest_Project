d3548340a7e1cdbf63e8d2cded3964c4867690fc
RimeLahkakCY <166617045+RimeLahkakCY@users.noreply.github.com>
2024-05-25 19:12:54 +0200

    Delete calcule_ratio.c

--- calcule_ratio.c
+++
@@ -1,7 +0,0 @@
-// Fonction pour calculer le ratio de réservation
-double calculerRatio(int nb_sieges_reserves, int nb_sieges_total) {
-    if (nb_sieges_total == 0) {
-        return 0.0; // Pour éviter la division par zéro
-    }
-    return ((double)nb_sieges_reserves / nb_sieges_total) * 100;
-}
